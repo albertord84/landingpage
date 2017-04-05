215303788ae96d208599cba81f8d56f9d4a13046
Yordano Pascual <yordanoweb@yahoo.es>
2017-04-05 15:39:39 -0400

    version portuguesa de la pagina

--- web/index.php
+++ web/index.php
@@ -30,7 +30,6 @@
             <span class="caret"></span>
           </a>
           <ul class="dropdown-menu" aria-labelledby="dropdownLang">
-            <!--<li><a href="#">PT - BR</a></li>-->
             <li><a href="#">EN - EU</a></li>
           </ul>
         </span>
@@ -48,25 +47,25 @@
               </div>
             </div>
             <div class="info col-xs-12 col-md-9 col-lg-9">
-              <p class="h4">&iexcl;Resultados reales de Instagram!</p>
-              <p class="h4">Revise su cuenta abajo:</p>
+              <p class="h4">Resultados reais para seu Instagram!</p>
+              <p class="h4">Analise sua conta abaixo:</p>
             </div>
           </div>
         </div> <!-- end top left contents -->
 
         <div class="right col-xs-12 col-sm-6 col-md-5 col-lg-5">
           <p class="flags"><img src="img/flags.png" /></p>
-          <p class="text-uppercase"><b>&iexcl;DUMBU es global!</b></p>
+          <p class="text-uppercase"><b>&iexcl;DUMBU &eacute; global!</b></p>
           <div class="small">
-            <p class="gray">Tenemos clientes en m&aacute;s de 200 pa&iacute;ses.</p>
-            <p class="gray">&iexcl;Aprovecha uno de los Startups que m&aacute;s crece en los &uacute;ltimos tiempos!</p>
+            <p class="gray">Temos clientes em mais de 200 pa&iacute;ses.</p>
+            <p class="gray">Fa&ccedil;a parte de uma das Startups que mais cresce nos &uacute;ltimos tempos!</p>
           </div>
         </div> <!-- end top right contents -->
 
       </div> <!-- end top contents -->
 
       <div class="middle-content row">
-        <h4 class="form-signin-heading text-center">Al verificar su cuenta ser&aacute; redireccionado a su sitio</h4>
+        <h4 class="form-signin-heading text-center">Ap&oacute;s analisar sua conta voc&ecirc; ser&aacute; direcionado para o site</h4>
       </div>
 
       <div class="middle-form row">
@@ -77,17 +76,17 @@
         </div>
         <div class="form-container col-xs-12 col-sm-6 col-sm-pull-1 col-md-7 col-md-push-1 col-lg-7">
           <form class="form-signin">
-            <h4 class="form-signin-heading text-center">Revise su Instagram aqu&iacute;</h4>
+            <h4 class="form-signin-heading text-center">Verifique seu instagram aqui</h4>
             <label for="inputEmail" 
-                   class="sr-only text-left">Usuario de Instagram</label>
-            <input type="email" id="inputEmail" 
+                   class="sr-only text-left">Usu&aacute;rio do Instagram</label>
+            <input type="text" id="inputEmail" 
                    class="form-control text-left" 
-                   placeholder="Usuario de Instagram" required autofocus>
-            <label for="inputPassword" class="sr-only">Contrase&ntilde;a</label>
-            <input type="password" id="inputPassword" class="form-control"
-                   placeholder="Contrase&ntilde;a" required>
+                   placeholder="Usu&aacute;rio do Instagram" required autofocus>
+            <label for="inputPassword" class="sr-only">E-mail</label>
+            <input type="email" id="inputPassword" class="form-control"
+                   placeholder="E-mail" required>
             <button class="btn btn-lg btn-primary btn-block" 
-                    type="submit">Verificar</button>
+                    type="submit">Analisar</button>
           </form>
         </div>
       </div>
@@ -95,7 +94,7 @@
       <div class="footer row text-center">
         <div class="col-xs-12 col-sm-12 col-md-12 col-lg-12">
           <p><img src="img/logo-white-plus.png" /></p>
-          <p class="text-uppercase">DUMBU 2017 - Todos los derechos reservados</p>
+          <p class="text-uppercase">DUMBU 2017 - TODOS OS DIREITOS RESERVADOS</p>
         </div>
       </div>
 
